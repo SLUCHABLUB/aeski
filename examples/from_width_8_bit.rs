4d726d532502be462ad07bd81bca9cf9cad6d72e
SLUCHABLUB <81807027+SLUCHABLUB@users.noreply.github.com>
2024-09-05 10:07:23 +0200

    Added ansi 8 bit example

---
+++ examples/from_width_8_bit.rs
@@ -0,0 +1,33 @@
+use aeski::color::ansi_8_bit::Ansi8Bit;
+use aeski::font::Font;
+use aeski::image::AsciiImage;
+use image::ImageReader;
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
+
+const GRADIENT: [char; 10] = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+const COVERAGE: f64 = 0.3;
+const ASPECT_RATIO: f64 = 0.5;
+
+fn input(prompt: &str) -> std::io::Result<String> {
+    print!("{prompt}");
+    stdout().flush()?;
+
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let font = Font::new_float(GRADIENT, COVERAGE, ASPECT_RATIO).unwrap();
+
+    let image_path = input("Path to image: ")?;
+    let image = ImageReader::open(image_path.trim())?.decode()?;
+
+    let width = input("Width in characters: ")?.trim().parse()?;
+    let ascii_image = AsciiImage::<Ansi8Bit>::from_image_with_width(&image, &font, width);
+
+    println!("{ascii_image}");
+
+    Ok(())
+}
